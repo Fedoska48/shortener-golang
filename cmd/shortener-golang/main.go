fe0ad338dc6d82169eb1c43ba197295b9281de02
Nikita Fedyaev <nikitafedyaev@MacBook-Pro-Nikita.local>
2024-07-10 00:02:02 +0300

    config init

---
+++ cmd/shortener-golang/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: clearenv
+
+	// TODO: init logger: slog: log/slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, chi render
+
+	// TODO: init run server
+}
