52761ec443a943b23bb5248371d5dd7c86abe893
Nikita <itsfedos@gmail.com>
2024-07-14 17:10:42 +0300

    storage and middlewares

--- cmd/shortener-golang/main.go
+++ cmd/shortener-golang/main.go
@@ -4,6 +4,12 @@ import (
 	"log/slog"
 	"os"
 	"shortener-golang/internal/config"
+	"shortener-golang/internal/http-server/logger"
+	"shortener-golang/internal/lib/logger/sl"
+	"shortener-golang/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -17,15 +23,30 @@ func main() {
 
 	cfg := config.MustLoad()
 
+	// TODO: init logger: slog: log/slog
 	log := setupLogger(cfg.Env)
 
 	log.Info("Runnning Shortener", slog.String("env", cfg.Env))
-	log.Debug("Debug level")
-	// TODO: init logger: slog: log/slog
 
 	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
 
-	// TODO: init router: chi, chi render
+	_ = storage
+	if err != nil {
+		log.Error("fail to init storage", sl.Err(err))
+		os.Exit(1)
+		// or empty return
+	}
+
+	router := chi.NewRouter()
+	// get request id
+	router.Use(middleware.RequestID)
+	// request logger
+	router.Use(logger.New(log))
+	// recover panic
+	router.Use(middleware.Recoverer)
+	// get url params
+	router.Use(middleware.URLFormat)
 
 	// TODO: init run server
 }
